37a5284171533a29d0e6626e1465a4cc028dcc11
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-12-20 12:53:39 +0530

    collision of the paddle added

--- script.js
+++ script.js
@@ -20,12 +20,20 @@ document.addEventListener("DOMContentLoaded",()=>{
         ball.style.left=`${ballX}px`; 
         ball.style.top=`${ballY}px`; 
 
+        //collision of ball and paddle
+
+        if(ballX<paddle.offsetLeft+paddle.offsetWidth &&
+             ballY>paddle.offsetTop 
+            && ballY-ball.offsetHeight<paddle.offsetTop+paddle.offsetHeight){
+            dx*=-1;
+        }
+
         if(ballX>table.offsetWidth-ball.offsetWidth || ballX<=0) dx*=-1;
-        if(ballY>table.offsetHeight-ball.offsetHeight || ballY<=0) dy*=-1;
+        if(ballY>table.offsetHeight-ball.offsetHeight || ballY<=0) dy*=-1;        
     },1);
 
     let paddleY=0;
-    let dPy=5; //displacement for paddle in Y direction
+    let dPy=10; //displacement for paddle in Y direction
     document.addEventListener("keydown",(event)=>{
         if(event.keyCode==38 && paddleY>0){
             paddleY+=(-1)*dPy;
@@ -34,6 +42,6 @@ document.addEventListener("DOMContentLoaded",()=>{
             paddleY+=dPy;  
         }
         paddle.style.top=`${paddleY}px`;
-    })
+    });
 
 });

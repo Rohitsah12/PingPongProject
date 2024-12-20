5d5b47a16fcc97f836ca668d60d1294953cf6432
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-12-20 12:44:19 +0530

    Making the paddle move

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded",()=>{
     let table=document.getElementById("ping-pong-table")
     let ball=document.getElementById("ball");
+    let paddle=document.getElementById("paddle");
+
+
     //Here the ball x and ball y will be helping to set a starting point of ball w.r.t table
     let ballX=10;//distance of the top of the ball w.r.t. ping pong table
     let ballY=10;//distance of the left of the ball w.r.t. ping pong table
@@ -19,5 +22,18 @@ document.addEventListener("DOMContentLoaded",()=>{
 
         if(ballX>table.offsetWidth-ball.offsetWidth || ballX<=0) dx*=-1;
         if(ballY>table.offsetHeight-ball.offsetHeight || ballY<=0) dy*=-1;
-    },1)
+    },1);
+
+    let paddleY=0;
+    let dPy=5; //displacement for paddle in Y direction
+    document.addEventListener("keydown",(event)=>{
+        if(event.keyCode==38 && paddleY>0){
+            paddleY+=(-1)*dPy;
+        }else if(event.keyCode==40 && paddleY<table.offsetHeight-paddle.offsetHeight){
+            //down arrow
+            paddleY+=dPy;  
+        }
+        paddle.style.top=`${paddleY}px`;
+    })
+
 });
